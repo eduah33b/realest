ee971b98b869b190f20b764dc90b9b0152f20c12
eduah33b <eduah33@hotmail.com>
2015-08-01 12:47:14 +0000

    multipages

---
+++ 1/js/index.js
@@ -0,0 +1,35 @@
+$( document ).ready(function(){
+	$('.slider#promo').slider({full_width: true, indicators: true, interval: 15000});
+	$('.slider#logo').slider({full_width: true, indicators: false, height: 240, interval: 10000});
+	$('.tooltipped').tooltip({delay: 1});
+	
+
+	$('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+	if($(window).width() > 992){
+		$(".button-collapse").sideNav({
+	      	menuWidth: 260, // Default is 240
+	      	edge: 'left',
+	      	closeOnClick: false
+	    });
+	}else{
+		$(".button-collapse").sideNav({
+	      	menuWidth: 260, // Default is 240
+	      	edge: 'left',
+	      	closeOnClick: true
+	    });
+	}
+
+	function go_to_this(id) {
+		$('html, body').animate({ scrollTop: document.getElementById(id).offsetTop + 'px'}, 1100);
+	}
+	go_to = go_to_this;
+
+	/*setTimeout(function(){ Materialize.toast('<span onclick="go_to(\'promop\');rem_i(\'#toast-container\')">Special Offer</span><a class="btn-flat yellow-text" onclick="rem_i(\'#toast-container\')"> Dismiss<a>', 100000); }, 1000);
+
+	function remove_item(uid) {
+		$(uid).html('');
+	}
+	rem_i = remove_item;*/
+
+});
