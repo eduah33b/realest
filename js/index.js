e531c55d655f1fb37a551bd92fa2112371005a18
eduah33b <eduah33@hotmail.com>
2015-10-29 13:52:47 +0000

    All Commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
-	$('.slider#promo').slider({full_width: true, indicators: true, interval: 15000});
+	$('.slider#promo').slider({full_width: true, indicators: false, interval: 15000});
 	$('.slider#logo').slider({full_width: true, indicators: false, height: 240, interval: 10000});
 	$('.tooltipped').tooltip({delay: 1});
 	
@@ -9,13 +9,21 @@ $( document ).ready(function(){
 	$(".button-collapse").sideNav({
       	menuWidth: 300, // Default is 240
       	edge: 'left',
-      	closeOnClick: true
+      	closeOnClick: false
     });
 
 	function go_to_this(id) {
 		$('html, body').animate({ scrollTop: document.getElementById(id).offsetTop + 'px'}, 1100);
 	}
 	go_to = go_to_this;
+
+	function attach_anim(){
+		var a_types = ['fadeInUpShort','bounceInUp', 'lightSpeedInLeft', 'bounceInRight', 'fadeInUp', 'growIn', 'flipInY', 'fadeInRightShort', 'fadeInUpShort', 'fadeInDownShort', 'flipInX', 'lightSpeedInDown', 'lightSpeedInUp', 'fadeInLeftShort'];
+		$.each($('.animatedParent *:not(div, img, .blink)'), function(i, val){
+			$(val).addClass('slower animated ' + a_types[i % a_types.length]).attr('data-id', i);
+		});
+	}
+	attach_anim();
 
 	/*setTimeout(function(){ Materialize.toast('<span onclick="go_to(\'promop\');rem_i(\'#toast-container\')">Special Offer</span><a class="btn-flat yellow-text" onclick="rem_i(\'#toast-container\')"> Dismiss<a>', 100000); }, 1000);
 
